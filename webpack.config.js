cb9a674590e4b1d451582c12d4a7e4d566e6dcde
Rinat Hairulin <Lipash73@gmail.com>
2023-01-23 14:58:53 +0500

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const path = require("path");
 
 module.exports = {
@@ -9,6 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "index.[contenthash:10].js",
+    assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
   },
   module: {
     rules: [
@@ -16,6 +19,30 @@ module.exports = {
         test: /\.js$/,
         use: "babel-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/resource",
+        generator: {
+          filename: path.join("icons", "[name].[contenthash][ext]"),
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
@@ -26,12 +53,39 @@ module.exports = {
         onStart: {
           delete: ["dist"],
         },
-        onEnd: {},
+        onEnd: {
+          copy: [
+            {
+              source: path.join("src", "static"),
+              destination: "dist",
+            },
+          ],
+        },
       },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:10].css",
     }),
   ],
   devServer: {
     watchFiles: path.join(__dirname, "src"),
     port: 9000,
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              ["svgo", { name: "preset-default" }],
+            ],
+          },
+        },
+      }),
+    ],
   },
 };
